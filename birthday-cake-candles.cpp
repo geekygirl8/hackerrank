c9ef709b65eae0d7ae8d3253129735e6b7cd84f1
Nikita <nikita871gidmail@gmail.com>
2020-10-01 23:31:20 +0530

    fixed error

--- birthday-cake-candles.cpp
+++ birthday-cake-candles.cpp
@@ -7,13 +7,3 @@ int main(){
     std::cout << c;
     return 0;
 }
-#include<iostream>
-
-int main(){
-    int c, n, max = 0;
-    std::cin.ignore();
-    while(std::cin >> n)
-        max < n ? c = !!(max = n) : c += max == n;
-    std::cout << c;
-    return 0;
-}
